1a0e0935049fc6d5e4da6e9c9e8af2d1a564f145
tsachilley <tsachille@126.com>
2025-07-01 16:52:47 +0800

    V0.1

---
+++ Htg.Hic.EE4Show/Htg.Hic.EE4Show/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Htg.Hic.EE4Show.Modules.ModuleName;
+using Htg.Hic.EE4Show.Services;
+using Htg.Hic.EE4Show.Services.Interfaces;
+using Htg.Hic.EE4Show.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace Htg.Hic.EE4Show;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+	protected override Window CreateShell()
+	{
+		return Container.Resolve<MainWindow>();
+	}
+
+	protected override void RegisterTypes(IContainerRegistry containerRegistry)
+	{
+		containerRegistry.RegisterSingleton<IMessageService, MessageService>();
+	}
+
+	protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+	{
+		moduleCatalog.AddModule<ModuleNameModule>();
+	}
+}
---
+++ Htg.Hic.EE4Show/Services/Htg.Hic.EE4Show.Services.Interfaces/IMessageService.cs
@@ -0,0 +1,7 @@
+﻿namespace Htg.Hic.EE4Show.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        string GetMessage();
+    }
+}
